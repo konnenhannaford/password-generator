61bf561e23e35744f2f12df727fddfb6355575d7
konnenhannaford <konnen23@gmail.com>
2021-09-09 11:28:44 +1000

    developed java to include var and functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,6 @@
 // characters taken from - https://www.rapidtables.com/code/text/ascii-table.html
 
 // my code - setting info
-var lower, upper, special, numbers, characters, prompt 
 var lower = ["a", "b", "c", "d", "e", "f","g","h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upper = ["A", "B", "C", "D", "E", "F","G","H","I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var special = ["!", '"', "#","$","%","&","’","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[",];
@@ -15,70 +14,91 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var characters = prompt("1. Welcome to your password Generator\n" +
+  var characters = confirm ("1. Welcome to your password Generator\n" +
                               
                           "Want to creat a password? Great, make sure all passwords contain at least 1 symbol, 1 number, 1 uppercase letter and 1 lowercase letter. Please select the amont of characters you would like to use");
 
 
   if ((characters < 8)||(characters > 128)) {
 
-      characters = true ("FAILED - Passwords are required to be between 8 and 128 characters long. Try again?");
-        
-      else if characters = false ("Ok thats great.);
-    }
-    
-  lower =     prompt ("2.  Lower case\n" +
-                      "Password requires lower case letters. Please select at least 1 of these options.\n" +);
-
-
-  upper =     prompt ("2.  Upper case\n" +
-                      "Password requires upper case letters. Please select at least 1 of these options.\n" +);
+      alert("FAILED - Passwords are required to be between 8 and 128 characters long. Try again?")
+      return;
+          }
   
+  var hasUpper = confirm ("click to include uppercase")
+  var hasLower= confirm ("click to include lowercase")
+  var hasNumber = confirm ("click to include numbercase")
+  var hasSpecial = confirm ("click to include specialcase")
   
-  numbers =     prompt ("2.  Numbers\n" +
-                      "Password requires numbers. Please select at least 1 of these options.\n" +);
+  if (hasUpper === false && hasLower === false && hasNumber === false  && hasSpecial || false )) {
+    alert = ("please choose one of these options")
+    return}
 
+var passwordchoice = {
 
-  characters =   prompt ("2.  Upper case\n" +
-                        "Password requires upper case letters. Please select at least 1 of these options.\n" +);
+  haslength: haslength;
+  hasupper: hasupper;
+  haslower: haslower;
+  hasnumber: hasnumber;
+  hasSpecial: hasSpecial};
+
+}
+return: passwordchoice
+
+}
+
+// establishing
+function pickRandomElementFromArray (randomArray) {
+  var randomIndex = Math.floor(Math.random() * randomArray.length);
+  return randomArray[randomIndex];
+}
+
+// invoking
+function generatepassword (){
+  var choice = writepass()
+  ()
+
+  if (choice.hasupper){
+    upper .concat (upper)
+    number .concat (upper)
+    special .concat (upper)
+    lower  ... push (pickRandomElementFromArray(upper))
+ 
+  }
+
+  if (choice.haslower){
+    upper.concat (lower)
+    number .concat (lower)
+    special .concat (lower)
+    lower  ... push (pickRandomElementFromArray(lower))
+ 
+  }
+  if (choice.hasnumber){
+    upper .concat (number)
+    number .concat (number)
+    special .concat (number)
+    lower  ... push (pickRandomElementFromArray(number))
+ 
+  }
+
+  if (choice.hasSpecial){
+    upper.concat (Special)
+    number .concat (Special)
+    special .concat (Special)
+    lower  ... push (pickRandomElementFromArray(Special))
+ 
+  }
+
   
-
-  if ((lower false || upper false  || number false  || characters || false )) {
-    
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
-
 // // 
 
-// // this is from the tute on friday
-// function pickRandomElementFromArray (randomArray) {
-//   var randomIndex = Math.floor(Math.random() * randomArray.length);
-//   return randomArray[randomIndex];
-// }
-// // this is from the tute on friday
-
-
-
-
-
-// // from another
-  // for(var i=0; i < lower.length; i++) {
-  //   // This statement will run each time the loop is executed
-  //   console.log("you havent satisfied criteria, please choose another option, " + students[i] + "!");
-  // }
-// // from another
-
-
-
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+passwordText.value = password;
 
