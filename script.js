504fe182485860b70e22e2480c25416a536c3595
konnenhannaford <konnen23@gmail.com>
2021-09-09 23:27:47 +1000

    updated java to remove extra script and meet criteria of only 1 criteria needing to be met

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var characters = window.prompt ("Welcome to your password Generator.\n ########################### \n Want to create a password? \n Great, make sure all passwords contain at least \n 1 symbol \n 1 number \n 1 uppercase letter \n 1 lowercase letter \n Please select the amont of characters you would like to use")
+  var characters = window.prompt ("Welcome to your password Generator.\n ########################### \n Want to create a password? \n Great, make sure all passwords contain at least \n 1 symbol or\n 1 number or\n 1 uppercase letter or\n 1 lowercase letter\n Please select the amont of characters you would like to use")
   if ((characters < 8) || (characters > 128)) {
 
     alert("FAILED - Passwords are required to be between 8 and 128 characters long. Try again?");
